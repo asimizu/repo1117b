3fe713d0f210b7d20cb46ba1abae4205ea92a8a9
asimizu <164690371+asimizu@users.noreply.github.com>
2024-11-17 13:38:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,40 @@ from linebot.v3.webhooks import (
 	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
 )
 import os
+import smtplib
+from email.mime.text import MIMEText
+import json
+
+def send_gmail(mail_from, mail_to, mail_subject, mail_body):
+
+
+    """ メッセージのオブジェクト """
+    msg = MIMEText(mail_body, "plain", "utf-8")
+    msg['Subject'] = mail_subject
+    msg['From'] = mail_from
+    msg['To'] = mail_to
+
+    # エラーキャッチ
+    try:
+        """ SMTPメールサーバーに接続 """
+        smtpobj = smtplib.SMTP('smtp.gmail.com', 587)  # SMTPオブジェクトを作成。smtp.gmail.comのSMTPサーバーの587番ポートを設定。
+        smtpobj.ehlo()                                 # SMTPサーバとの接続を確立
+        smtpobj.set_debuglevel(True) # enable debug output
+        smtpobj.starttls()                             # TLS暗号化通信開始
+        gmail_addr = "yamaharu9876@gmail.com"       # Googleアカウント(このアドレスをFromにして送られるっぽい)
+        app_passwd = "bxvykhiunzvojfvk"       # アプリパスワード
+        smtpobj.login(gmail_addr, app_passwd)          # SMTPサーバーへログイン
+
+        """ メール送信 """
+        smtpobj.sendmail(mail_from, mail_to, msg.as_string())
+
+        """ SMTPサーバーとの接続解除 """
+        smtpobj.quit()
+
+    except Exception as e:
+        print(e)
+    
+    return "メール送信完了"
 
 ## .env ファイル読み込み
 from dotenv import load_dotenv
@@ -82,6 +116,16 @@ def handle_message(event):
 		replyToken=event.reply_token,
 		messages=[TextMessage(text=reply)]
 	))
+	""" メール設定 """
+	mail_from = "yamaharu9876@gmail.com"       # 送信元アドレス
+        mail_to = "asimizu@gmail.com"         # 送信先アドレス(To)
+	mail_subject = "from LINE"                   # メール件名
+	mail_body = event.message.text                      # メール本文
+	
+	""" send_gmail関数実行 """
+    	result = send_gmail(mail_from, mail_to, mail_subject, mail_body)
+	print(result)
+
 
 ## 起動確認用ウェブサイトのトップページ
 @app.route('/', methods=['GET'])
